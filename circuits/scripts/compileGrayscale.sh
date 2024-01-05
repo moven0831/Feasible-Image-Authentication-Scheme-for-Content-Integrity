be860e1611acf085b517b0f9cdc55d2f69e7e885
moven0831 <moven0831@gmail.com>
2024-01-05 21:22:33 +0800

    feat(compileGrayscale): allow arbitrary arguments

---
+++ circuits/scripts/compileGrayscale.sh
@@ -0,0 +1,5 @@
+cd circuits/circuits
+head -n -1 grayscale.circom > grayscale_benchmark.circom
+echo "component main = GrayscaleChecker($1, $2);" >> grayscale_benchmark.circom
+circom grayscale_benchmark.circom --r1cs --sym --c --prime vesta --output ../dist/zkSnarkBuild
+cd ../dist/zkSnarkBuild/grayscale_benchmark_cpp && make
