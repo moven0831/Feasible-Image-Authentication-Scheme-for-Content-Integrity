c0474a7de7d58d2b3e340807643fe4a6e92a7b28
moven0831 <moven0831@gmail.com>
2024-01-07 19:45:30 +0800

    refactor(crop):  prevent from large CRS

--- src/bin/crop_preprocess.rs
+++ src/bin/crop_preprocess.rs
@@ -1,14 +1,14 @@
 use image::{GenericImageView, imageops};
 
 fn main() {
-    let image_name = "c2pa_mini";
+    let image_name = "c2pa_tiny";
     let image_path = format!("src/bin/assets/{}.jpg", image_name);
     let mut img = image::open(image_path).unwrap();
 
-    let crop_x = 200;
-    let crop_y = 200;
-    let crop_width = 100;
-    let crop_height = 100;
+    let crop_x = 50;
+    let crop_y = 50;
+    let crop_width = 10;
+    let crop_height = 10;
 
     let cropped_image = imageops::crop(&mut img, crop_x, crop_y, crop_width, crop_height).to_image();
     assert!(cropped_image.dimensions() == (crop_width, crop_height));
