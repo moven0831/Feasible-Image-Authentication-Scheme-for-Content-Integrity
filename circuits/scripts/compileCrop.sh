7e4b95690bd7db86868d5f4c8ea27d2c851a8048
moven0831 <moven0831@gmail.com>
2024-01-08 22:56:07 +0800

    fix(compileCircom): head cmd might fail, fix with sed

--- circuits/scripts/compileCrop.sh
+++ circuits/scripts/compileCrop.sh
@@ -1,5 +1,5 @@
 cd circuits/circuits
-head -n -1 crop.circom > crop_benchmark.circom
+sed '$d' crop.circom > crop_benchmark.circom
 echo "component main = Crop($1, $2, $3, $4, $5, $6);" >> crop_benchmark.circom
 circom crop_benchmark.circom --r1cs --sym --c --prime vesta --output ../dist/zkSnarkBuild
 cd ../dist/zkSnarkBuild/crop_benchmark_cpp && make
