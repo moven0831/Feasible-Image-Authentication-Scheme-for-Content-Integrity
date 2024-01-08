5989dbde733f326a927cd38995c170241de79288
moven0831 <moven0831@gmail.com>
2024-01-08 22:56:52 +0800

    fix(squares): head cmd might fail, fix with sed

--- circuits/scripts/compileSquares.sh
+++ circuits/scripts/compileSquares.sh
@@ -1,5 +1,5 @@
 cd circuits/circuits
-head -n -1 squares.circom > squares_benchmark.circom
+sed '$d' squares.circom > squares_benchmark.circom
 echo "component main = Bright($1);" >> squares_benchmark.circom
 circom squares_benchmark.circom --r1cs --sym --c --prime vesta --output ../dist/zkSnarkBuild
 cd ../dist/zkSnarkBuild/squares_benchmark_cpp && make
