b2adb2be1d224f0d2eb3e3290629279539a872cd
moven0831 <moven0831@gmail.com>
2024-01-08 22:56:23 +0800

    fix(grayscale): head cmd might fail, fix with sed

--- circuits/scripts/compileGrayscale.sh
+++ circuits/scripts/compileGrayscale.sh
@@ -1,5 +1,5 @@
 cd circuits/circuits
-head -n -1 grayscale.circom > grayscale_benchmark.circom
+sed '$d' grayscale.circom > grayscale_benchmark.circom
 echo "component main = GrayscaleChecker($1, $2);" >> grayscale_benchmark.circom
 circom grayscale_benchmark.circom --r1cs --sym --c --prime vesta --output ../dist/zkSnarkBuild
 cd ../dist/zkSnarkBuild/grayscale_benchmark_cpp && make
