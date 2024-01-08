e7f31b618b2ec663584dc293d30cd128e2a0bf3a
moven0831 <moven0831@gmail.com>
2024-01-08 22:56:36 +0800

    fix(grayscaleNoRound): head cmd might fail, fix with sed

--- circuits/scripts/compileGrayscaleNoRound.sh
+++ circuits/scripts/compileGrayscaleNoRound.sh
@@ -1,5 +1,5 @@
 cd circuits/circuits
-head -n -1 grayscale_no_round.circom > grayscale_no_round_benchmark.circom
+sed '$d' grayscale_no_round.circom > grayscale_no_round_benchmark.circom
 echo "component main = GrayscaleChecker($1, $2);" >> grayscale_no_round_benchmark.circom
 circom grayscale_no_round_benchmark.circom --r1cs --sym --c --prime vesta --output ../dist/zkSnarkBuild
 cd ../dist/zkSnarkBuild/grayscale_no_round_benchmark_cpp && make
